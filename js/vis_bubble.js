f20612add905eb99af4140d8d7a7f5a4303b89d1
ethanliu24 <liu.ethan2005@gmail.com>
2025-10-24 15:42:31 -0400

    add bubble baselines for vis

---
+++ js/vis_bubble.js
@@ -0,0 +1,95 @@
+class BubblePlayerViz {
+  constructor({ selector, data, centerSelector }) {
+    this.svg = d3.select(selector);
+    this.centerEl = document.querySelector(centerSelector);
+    this.data = data;
+    this.init();
+    this.render();
+    this.attachResizeHandler();
+  }
+
+  init() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+
+    this.svg.attr("viewBox", [0, 0, this.width, this.height]);
+
+    const rect = this.centerEl.getBoundingClientRect();
+    this.centerX = this.width / 2;
+    this.centerY = window.scrollY + rect.top + rect.height / 2;
+
+    this.nodes = [
+      { id: "center", isCenter: true, fx: this.centerX, fy: this.centerY }, // invisible node to push nodes away
+      ...this.data
+    ];
+
+    this.simulation = d3.forceSimulation(this.nodes)
+      .force("charge", d3.forceManyBody().strength(-60))
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
+      .force("collision", d3.forceCollide().radius(d => d.value))
+      .on("tick", () => this.ticked());
+  }
+
+  render() {
+    this.circles = this.svg.selectAll("circle")
+      .data(this.nodes.filter(d => !d.isCenter), d => d.id)
+      .join("circle")
+      .attr("r", d => d.tempo / 1.4)
+      .attr("fill", (_d, i) => d3.schemeCategory10[i % 10])
+      .attr("opacity", 0.8)
+      .attr("class", "bubble-viz-bubble");
+
+    this.labels = this.svg.selectAll("text")
+      .data(this.nodes.filter(d => !d.isCenter), d => d.id)
+      .join("text")
+      .text("▶")
+      .attr("text-anchor", "middle")
+      .attr("dy", ".35em")
+      .attr("class", "unselectable bubble-viz-bubble");
+  }
+
+  ticked() {
+    this.circles
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+
+    this.labels
+      .attr("x", d => d.x)
+      .attr("y", d => d.y);
+  }
+
+  updateData(newData) {
+    this.simulation.stop();
+
+    this.data = newData;
+    this.nodes = [
+      { id: "center", isCenter: true, fx: this.centerX, fy: this.centerY },
+      ...this.data
+    ];
+
+    this.simulation = d3.forceSimulation(this.nodes)
+      .force("charge", d3.forceManyBody().strength(-40))
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
+      .force("collision", d3.forceCollide().radius(d => d.value))
+      .on("tick", () => this.ticked());
+
+    this.render();
+  }
+
+  attachResizeHandler() {
+    window.addEventListener("resize", () => {
+      this.simulation.stop();
+      this.svg.selectAll("*").remove();
+      this.init();
+      this.render();
+    });
+  }
+}
+
+d3.csv("data/processed/temp_dataset.csv").then((data) => {
+  new BubblePlayerViz({
+    selector: "#bubble-viz",
+    centerSelector: "#hook",
+    data: data.slice(0, 10)
+  });
+})
